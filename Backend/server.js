0c00111ed82db20c37d2c3da34359a4b500f279f
Anouar <anouar.soufyani@gmail.com>
2024-10-29 00:10:44 +0100

    add comment in server

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 app.use(express.json()); // for parsing application/json, to parse request body
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser()); // for parsing cookie headers
 
 app.use('/api/auth', authRoutes )
 
